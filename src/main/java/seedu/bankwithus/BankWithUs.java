cf0843a6bb0b323e5eb62c53dbb9b19819ebb682
tyuyang <tayyuyang@gmail.com>
2023-03-10 15:15:10 +0800

    Create skeleton code for BankWithUs

---
+++ src/main/java/seedu/bankwithus/BankWithUs.java
@@ -0,0 +1,28 @@
+package seedu.bankwithus;
+
+public class BankWithUs {
+
+    private Storage storage;
+    private Ui ui;
+    private AccountList accounts;
+
+    public static final String FILE_PATH = "data/save.txt";
+
+    public BankWithUs(String filePath) {
+        ui = new UI();
+        storage = new Storage();
+        try {
+            accounts = new AccountList(storage.load());
+        } catch (Exception e) {
+            ui.showLoadingError();
+            accounts = new AccountList();
+        }
+    }
+
+    public void run() {
+
+    }
+    public static void main(String[] args) {
+        new BankWithUs(FILE_PATH).run();
+    }
+}
