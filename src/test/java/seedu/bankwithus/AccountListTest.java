e290bf28b9f7e4a5d919ac13b48237c3112d7d86
xiaoge26 <liuxiaoge0206@gmail.com>
2023-03-12 21:24:59 +0800

    Fix Checkstyle errors

--- src/test/java/seedu/bankwithus/AccountListTest.java
+++ src/test/java/seedu/bankwithus/AccountListTest.java
@@ -1,7 +1,7 @@
 package seedu.bankwithus;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AccountListTest {
 
