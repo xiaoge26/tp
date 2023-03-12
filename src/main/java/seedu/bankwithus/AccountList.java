9a71ca5094d28d5b800dfea5c0ce2a88d4723891
xiaoge26 <liuxiaoge0206@gmail.com>
2023-03-12 11:06:07 +0800

    Add create account feature

--- src/main/java/seedu/bankwithus/AccountList.java
+++ src/main/java/seedu/bankwithus/AccountList.java
@@ -1,9 +1,23 @@
 package seedu.bankwithus;
 
+import java.util.ArrayList;
+
 public class AccountList {
+    protected ArrayList<Account> accounts;
 
     public AccountList() {
-        
+    }
+
+    /**
+     * Creates a new account and adds it to the AccountList.
+     *
+     * @param name the name of the new account to be added
+     * @param balanceString the balance of the new account to be added in String type
+     */
+    public void addAccount(String name, String balanceString) {
+        float balance = Float.parseFloat(balanceString);
+        Account newAccount = new Account(name, balance);
+        accounts.add(newAccount);
     }
     
     public AccountList(String saveString) {
