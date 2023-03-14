6fee70a5d3fc7906d3bc482584f4e7dc9689d720
Sherlock-YH <yh1259891725@gmail.com>
2023-03-14 14:38:43 +0800

    Add parseSavedFile

--- src/main/java/seedu/bankwithus/Parser.java
+++ src/main/java/seedu/bankwithus/Parser.java
@@ -2,6 +2,8 @@ package seedu.bankwithus;
 
 import seedu.bankwithus.exceptions.CommandNotFoundException;
 
+import java.util.Scanner;
+
 public class Parser {
 
     private BankWithUs bwu;
@@ -13,7 +15,7 @@ public class Parser {
     /**
      * Parses the user input into command and arguments.
      */
-    
+
     public void parseUserInput(String input) throws CommandNotFoundException {
         // Split input by space
         String[] split = input.trim().split("\\s+", 2);
@@ -21,11 +23,22 @@ public class Parser {
         String args = split.length == 2 ? split[1] : "";
 
         switch (command) {
-        case "exit":
-            bwu.isExitEntered = true;
-            break;
-        default:
-            throw new CommandNotFoundException();
+            case "exit":
+                bwu.isExitEntered = true;
+                break;
+            default:
+                throw new CommandNotFoundException();
+        }
+    }
+
+    public void parseSavedFile(AccountList list) {
+        Scanner myReader = new Scanner("data/save.txt");
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            String[] splitDetails = data.split("\\$");
+            String name = splitDetails[0];
+            String balance = splitDetails[1];
+            list.addAccount(name, balance);
         }
     }
 
