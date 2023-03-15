462490d2c96ad384dcb9e8463e2c225b0a48cc4c
vishnuvk47 <vishnu@vishnus-MacBook-Pro.local>
2023-03-15 21:28:07 +0800

    Added a JUNIT test for AccountList addAccount method

--- src/test/java/seedu/bankwithus/AccountListTest.java
+++ src/test/java/seedu/bankwithus/AccountListTest.java
@@ -1,6 +1,8 @@
 package seedu.bankwithus;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AccountListTest {
@@ -13,4 +15,13 @@ class AccountListTest {
         assertThrows(NullPointerException.class,
                 () -> accountList.addAccount(name, balanceString));
     }
+
+    @Test
+    void testing_Add_New_account_Jane_with_1000_balance() {
+        AccountList TestAccountList = new AccountList();
+        TestAccountList.addAccount("Jane", "1000");
+        assertEquals("Jane", TestAccountList.accounts.get(0).name);
+        assertEquals(1000.0, TestAccountList.accounts.get(0).balance);
+    }
+
 }
