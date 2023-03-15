36e01531805ea60763094fdc8d7e2e00a117b6fc
vishnuvk47 <vishnu@vishnus-MacBook-Pro.local>
2023-03-15 18:02:46 +0800

    Removed an unused import in storageTest

--- src/test/java/seedu/bankwithus/StorageTest.java
+++ src/test/java/seedu/bankwithus/StorageTest.java
@@ -2,8 +2,6 @@ package seedu.bankwithus;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Scanner;
-
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class StorageTest {
