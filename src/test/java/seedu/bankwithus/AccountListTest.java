22e5f6c6ab456b8eefe48be631656c624cb7c413
xiaoge26 <liuxiaoge0206@gmail.com>
2023-03-23 22:17:34 +0800

    Fix indentation errors

--- src/test/java/seedu/bankwithus/AccountListTest.java
+++ src/test/java/seedu/bankwithus/AccountListTest.java
@@ -1,22 +1,21 @@
 package seedu.bankwithus;
 
- import org.junit.jupiter.api.Test;
- import seedu.bankwithus.exceptions.NegativeAmountException;
+import org.junit.jupiter.api.Test;
+import seedu.bankwithus.exceptions.NegativeAmountException;
 
- import static org.junit.jupiter.api.Assertions.assertEquals;
- import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AccountListTest {
-
     @Test
-     void depositMoney_notANumber_expectException() {
-         String amountString = "abc";
-         String name = "Bob";
-         String balance = "0";
-         AccountList accountList = new AccountList();
-         accountList.addAccount(name, balance);
-         assertThrows(NumberFormatException.class,
-                 () -> accountList.depositMoney(amountString));
+    void depositMoney_notANumber_expectException() {
+        String amountString = "abc";
+        String name = "Bob";
+        String balance = "0";
+        AccountList accountList = new AccountList();
+        accountList.addAccount(name, balance);
+        assertThrows(NumberFormatException.class,
+                () -> accountList.depositMoney(amountString));
      }
 
     @Test
@@ -29,33 +28,32 @@ class AccountListTest {
         assertThrows(NegativeAmountException.class,
                 () -> accountList.depositMoney(amountString));
     }
+    @Test
+    void depositMoney_largeAmount_expectDeposit() {
+        String amountString = "12345678.85";
+        String name = "Bob";
+        String balance = "100.05";
+        AccountList accountList = new AccountList();
+        Ui ui = new Ui();
+        accountList.addAccount(name, balance);
+        try {
+            accountList.depositMoney(amountString);
+        } catch (NumberFormatException e) {
+            ui.showNumberFormatError();
+        } catch (NullPointerException e) {
+            ui.showNullInputError();
+        } catch (NegativeAmountException e) {
+            ui.showNegativeAmountError();
+        }
+        assertEquals("12345778.9", accountList.getMainAccount().getAccountBalance());
+    }
 
-     @Test
-     void depositMoney_largeAmount_expectDeposit() {
-         String amountString = "12345678.85";
-         String name = "Bob";
-         String balance = "100.05";
-         AccountList accountList = new AccountList();
-         Ui ui = new Ui();
-         accountList.addAccount(name, balance);
-         try {
-             accountList.depositMoney(amountString);
-         } catch (NumberFormatException e) {
-             ui.showNumberFormatError();
-         } catch (NullPointerException e) {
-             ui.showNullInputError();
-         } catch (NegativeAmountException e) {
-             ui.showNegativeAmountError();
-         }
-         assertEquals("12345778.9", accountList.getMainAccount().getAccountBalance());
-     }
 
-
-     @Test
-     void testingAddAccount() {
-         AccountList TestAccountList = new AccountList();
-         TestAccountList.addAccount("Jane", "1000");
-         assertEquals("Jane", TestAccountList.getMainAccount().getName());
-         assertEquals("1000", TestAccountList.getMainAccount().getAccountBalance());
-     }
+    @Test
+    void testingAddAccount() {
+        AccountList TestAccountList = new AccountList();
+        TestAccountList.addAccount("Jane", "1000");
+        assertEquals("Jane", TestAccountList.getMainAccount().getName());
+        assertEquals("1000", TestAccountList.getMainAccount().getAccountBalance());
+    }
 }
